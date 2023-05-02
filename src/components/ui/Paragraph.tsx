e8cacab3f4e5037ae350cc9dad116ffa2abd403b
Nghi Nguyen <nghi.nguyen@evehr.com>
2023-05-02 23:23:12 +0700

    Fix: setup & config

--- src/components/ui/Paragraph.tsx
+++ src/components/ui/Paragraph.tsx
@@ -1,8 +1,6 @@
-import React, {FC, HTMLAttributes, forwardRef} from 'react'
+import React, { FC, HTMLAttributes, forwardRef } from 'react'
 import {VariantProps, cva} from 'class-variance-authority'
-import { cn } from '@/lib/util'
-
-
+import { cn } from '@/lib/utils'
 
 const paragraphVariants = cva(
   'max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center',
